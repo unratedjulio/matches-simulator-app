4c916334817142d8173801c2517a364a5f47fd79
Julio Amorim <julio_cesar_antonio@hotmail.com>
2022-06-15 12:51:26 -0300

    Desenvolvimento dos Layouts de Listagem e detalhe de partidas.

--- app/src/main/java/com/example/simulator/DetailActivity.kt
+++ app/src/main/java/com/example/simulator/DetailActivity.kt
@@ -1,9 +1,15 @@
 package com.example.simulator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simulator.databinding.ActivityDetailBinding
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    @SuppressLint("UseSupportActionBar")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
